e59cf3b911b2110d73516155386579a962e6b8da
Marko Cotra <cotra.marko@gmail.com>
2018-11-04 21:52:05 +0100

    Add endpont for test response

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
+
+
+st = {
+    'wind': '4 m/s',
+    'wind direction': 'NW',
+    'Air density': 42.2,
+    'Projectile circular radius [m]': 0.25,
+}
 
 
 @app.route('/')
 def hello_world():
     return 'Hello, Nanette!'
+
+
+@app.route('/state')
+def state():
+    return jsonify(st)
